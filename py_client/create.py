d60fd6e32e899a6a5ac66e0f2842281432d14472
João Pedro Castro <sirjpcastro@gmail.com>
2024-02-16 14:22:21 -0300

    Implementa autenticação por tokens

--- py_client/create.py
+++ py_client/create.py
@@ -1,10 +1,11 @@
 import requests
 
+headers={'Authorization': f'Bearer 0c3335dca07b94432455c525cfc14e44f23b832b'}
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
     "title": "This field is done"
 }
 
-get_response = requests.post(endpoint, json=data) # HTTP Request
+get_response = requests.post(endpoint, json=data, headers=headers) # HTTP Request
 print(get_response.json())
